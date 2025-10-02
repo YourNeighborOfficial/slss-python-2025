6ea701699817595656cd960d332d65e98d2824ce
Julian Sauvageau - 911 <email address>
2025-10-02 11:27:22 -0700

    Added CYOA to repository

---
+++ activity-cyoa-Julian Sauvageau.py
@@ -0,0 +1,82 @@
+# choose your own adventure
+# steve jobs
+# 24 september 2025
+
+
+print(" \"its a sunny day in Cupertino...\" ")
+print(" \"you can hear the talking of people and the birds in the sky...\" ")
+print(" \"you slowly open the door to your house...\"  ")
+print(" \"as you walk in, you see blood everywhere...\"  ")
+
+
+
+print(" \"you see a robber in your house, and he slimed out your dog and kids!\"   ")
+print(" \"he turns around and points his gun at you!\" ")
+print("HAHAHAH I JUST SLIMED OUT YOUR WHOLE FAMILY")
+C_pass = input("NOW GIVE ME THE PASSWORD TO YOUR CRYPTO ACCOUNT")
+print(f"{C_pass} didnt work bro")
+print(" \"he proceeds to steal your air fryer\"")
+user_choice = input(" \" do you fight back and prevent your air fryer from being stolen?\"")
+
+
+
+if user_choice == "yes":
+    print(" \"doesnt matter gng he shot your leg and now youre bleeding out\"")
+    print(" \"you wake up in the hospital...\"")
+    print(" \"a nurse walks in and hands you a paper saying your bill is $670000...\"")
+    rob = input(" \"do you end it all or rob a government research facility?\"")
+    if rob == "rob a government research facility":
+        print("\"GREAT choice! you slowly make your way out of the hospital and approach the research facility...\"")
+        print("\"you got cooked the firing squad shot yer goofy ahh the second you stepped into the building like what did you think would happen robbing a government building🙏🙏🙏\"")
+        print("\"you died brochacho its over. the end lmao\"")
+    else:
+        print("\"you took the easy way out like a LOSER and overdosed on fent that the doctor accidentally left by your bed\"")
+        print("\"gg its jover fro you\"")
+        print("\"just kidding they revived you somehow idk\"")
+        die = input("\"are you mad they didnt let you die\"")
+
+        if die == "yes":
+            print("you blew up the hospital with a hydrogen bomb and everyone died the end")
+
+        else:
+            print("thats crazy")
+            print("you continue recovering in the hospital and by the time you get out your bill is $670000000000000000 now")
+            abcd = input("can you afford that")
+
+            if abcd == "yes":
+                print("stop lying")
+                print("im just trying to entertain you by feeding you this masterpiece of a story and this is how you treat me, LYING")
+                print("im ending the story goodbye")
+
+            else:
+                print("thats too bad lmao")
+                print("since you cant pay your debt the government sent you to a slave mine in Africa where youll work 67 hours a day for the rest of your life")
+                print("the end")
+
+
+
+else:
+    print("\"you died lmao\"")
+    print("\"wait its not over you injected yourself with orange juice and youre back\"")
+    print("\"you lost your air fryer tho do you still want to live\"")
+    live = input("yes or no")
+    if live == "yes":
+        print("\"you dig yourself out of the grave\"" )
+        print("\"everyone around you starts screaming and shooting at you\"")
+        print("\"you died again but you can revive do you want to?\"")
+        input("yes or no")
+        input("can you say that again i didnt quite get it")
+        input("bro SPEAK UP nobody can hear you")
+        input("i cant hear you")
+        input("blah blah blah i cant hear you")
+        input("oewgrgowogogew;r;ogrewerg")
+        input("SPEAK UP")
+        input("gshtkgenogqonefon")
+        input("insert cash or select payment type")
+        input("01101010101010001010101")
+        # input("67676767676767")                                                                                                        nb
+        input("4141414141")
+        print("you pmo gng i aint reviving you anymore you died 🙏")
+        print("the end")
+    else:
+        print("ok lmao the end you died")
