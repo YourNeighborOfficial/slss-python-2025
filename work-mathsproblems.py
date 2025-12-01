a426b06bf89277f2b8195cdea5132f59a4f5c2fc
Julian Sauvageau - 911 <email address>
2025-12-01 10:59:08 -0800

    work-mathsproblems.py

---
+++ work-mathsproblems.py
@@ -0,0 +1,29 @@
+# Maths Problems
+# Julian Sauvageau
+#
+import os
+
+# problem 1:
+
+# def age_in_2049():
+# age = int(input(f"how old are you?"))
+# print(f"your age will be {age + 24} in 2049!")
+
+
+# def main():
+# age_in_2049()
+
+
+# if __name__ == "__main__":
+# main()
+
+
+# problem 2:
+
+ratings = []
+
+for i in range(5):
+    rating = float(input(f"please enter a rating from 1-10: "))
+    ratings.append(rating)
+
+print("All ratings:", ratings)
